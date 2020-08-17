8dbb2afe763156dd28fdb99b7f5906eb8b6054e5
ddos25 <ddos25@163.com>
2020-08-17 13:13:43 +0800

    'login'

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+      <el-container >
+        <el-header>Header</el-header>
+        <el-container>
+          <el-aside width="200px">Aside</el-aside>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped lang="less">
+  .home
+.el-header{
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+</style>
